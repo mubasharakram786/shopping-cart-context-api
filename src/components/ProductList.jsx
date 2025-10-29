8298905ac4f354a76ffdbd392b4070fdce90bfbc
mubasharakram786 <mubashar1418@gmail.com>
2025-10-29 18:22:12 +0500

    Fetching and display products on page and add proxy server

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductCard from './ProductCard'
+
+const ProductList = ({products}) => {
+  return (
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {products.map((product)=>(
+        <ProductCard product={product} key={product.id} />
+        ))}
+      </div>
+  )
+}
+
+export default ProductList
