1da659032642853b1ae3a94b0a261cb7773eb178
mubasharakram786 <mubashar1418@gmail.com>
2025-10-30 22:57:29 +0500

    Feat: Add to Cart Logic

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import { ProductProvider } from './context/ProductContext.jsx'
 import './index.css'
 import App from './App.jsx'
+import { CartProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <ProductProvider>
+    <CartProvider>
     <App />
+    </CartProvider>
   </ProductProvider>
 )
