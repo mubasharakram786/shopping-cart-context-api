9c2509ee5cada602e6efdec488943d0e52e3431b
mubasharakram786 <mubashar1418@gmail.com>
2025-11-01 16:27:52 +0500

    Add header for displaying items in the cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import Header from "./components/Header"
 import ProductList from "./components/ProductList"
 
 const App = () => {
 
  
   return (
+    <>
+    <Header/>  
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-bold mb-6">🛒 Product Catalog</h1>
       <ProductList />
     </div>
+    </>
   )
 }
 
